32428a4752e20887c0204c2a30ab6e3e9acae9b0
Ben Galloway <ben@bengalloway.io>
2018-09-27 23:06:12 +0100

    Disable service worker for use over HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./inc/index.css";
 import App from "./App";
-import registerServiceWorker from "./util/registerServiceWorker";
+//import registerServiceWorker from "./util/registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+//registerServiceWorker();
