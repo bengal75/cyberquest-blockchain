8e4fbf14d573ef9d82dec41e6d1996a05a67b394
Ben Galloway <ben@bengalloway.io>
2018-09-27 21:16:25 +0100

    Add some guidance to the changeable config file

--- public/config.js
+++ public/config.js
@@ -1,3 +1,17 @@
+// This must be changed for proper functioning in the workshop!
+// An IP address will do - something like
+// window.api = "http://192.168.0.5";
 window.api = "https://api.blockchain.bengalloway.io";
+//
+
+// There is an artificial delay introduced to the mining process
+// to prevent participants from overloading the server.
+// It can be adjusted here, in seconds.
 window.mineDelay = 10; //seconds
+
+// This determines how entered usernames are validated.
+// It probably doesn't need to be changed!
+// Currently this means:
+// "letters and numbers only, at least one of!
+// No spaces, no other (special) characters."
 window.usernameRegexp = /^[\w]+$/;
